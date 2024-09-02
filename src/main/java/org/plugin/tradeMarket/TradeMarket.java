33561a2d3c19e98a4d6147870e9242600a3a1667
Thomas <namulabre@gmail.com>
2024-09-02 15:56:46 +0200

    Initial commit

---
+++ src/main/java/org/plugin/tradeMarket/TradeMarket.java
@@ -0,0 +1,17 @@
+package org.plugin.tradeMarket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TradeMarket extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
